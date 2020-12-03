dd1acd5327beb3292c3a5ce0823e4a6c2e57d8eb
Calvin Li <calvin@Calvins-MacBook-Pro.local>
2020-12-03 15:25:56 +0100

    update on mandala

--- week_3/mandala.py
+++ week_3/mandala.py
@@ -2,9 +2,27 @@ import turtle
 
 paper = turtle.Screen()
 leonardo = turtle.Turtle()
+lucifer = turtle.Turtle()
+maze = turtle.Turtle()
+
 
 leonardo.shape("turtle")
 leonardo.pensize(5)
+leonardo.speed("fast")
+
+lucifer.shape("turtle")
+lucifer.pensize(2)
+
+
+lucifer.speed("fast")
+maze.setposition(0,190.2)
+maze.shape("turtle")
+maze.pensize(2)
+maze.speed("fast")
+
+
+
+
 
 colors = ["red", "orange", "green", "blue", "purple"]
 
@@ -17,6 +35,20 @@ for index in range(60):
     else:
         leonardo.forward(20)
     leonardo.left(6)
+
+for index in range(60):
+    lucifer.color(colors[index % len(colors)])
+    if index % 2 == 0:
+        lucifer.forward(20)
+    else:
+        lucifer.forward(10)
+    lucifer.left(9)
+print(lucifer.position())
+
+for index in range(20):
+    lucifer.color(colors[index % len(colors)])
+    lucifer.left(189)
+    lucifer.forward(30)
 
 
 paper.exitonclick()
