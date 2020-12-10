621e3f7cc57c587bcd4bac777c4d0e0a31ca337b
calvin <calvinli781@gmail.com>
2020-12-10 19:16:08 +0100

    demos

--- week_5/demos/5_3_lists.py
+++ week_5/demos/5_3_lists.py
@@ -1,7 +1,7 @@
 # string join
 week_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-print("\n".join(week_days))
+print(", ".join(week_days[:]))
 
 for day in week_days:
     print(day, "*", sep="")
