f59a230192c48a8a4a6729455e8b585ffe17224b
calvin <calvinli781@gmail.com>
2020-12-10 16:50:04 +0100

    week 3 exercises

---
+++ week_3/exercise 3_4_4_6.py
@@ -0,0 +1,19 @@
+numbers = [83, 75, 74.9, 69.9, 65, 69, 59.9, 55, 50, 49.9, 45, 44.9, 40, 39.9, 2, 0]
+
+
+for mark in numbers:
+    if mark >= 75:
+        grade = "First"
+    elif mark >= 70:
+        grade = "Upper Second"
+    elif mark >= 60:
+        grade = "Second"
+    elif mark >= 50:
+        grade = "Third"
+    elif mark >= 45:
+        grade = "F1 Supp"
+    elif mark >= 40:
+        grade = "F2"
+    else:
+        grade = "F3"
+    print(mark, grade)
---
+++ week_3/exercise 3_4_4_7.py
@@ -0,0 +1,8 @@
+import math
+
+side_1 = float(input("What is the length of 1 side?"))
+side_2 = float(input("What is the length of the other side?"))
+
+hypotenuse = (side_1 ** 2 + side_2 ** 2) ** 0.5
+
+print("Your hypotenuse is", hypotenuse)
