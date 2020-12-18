7cdfe4268d5ea0dd7caf70c2493103f2649265b7
calvin <calvinli781@gmail.com>
2020-12-18 16:40:09 +0100

    week 3 exercises

---
+++ week_3/third_10/exercise 3_4_4_11.py
@@ -0,0 +1,7 @@
+numbers = [63, 112, 14, 28, 916]
+count = 0
+
+for number in numbers:
+    if number % 2 == 0:
+        count += 1
+print(count)
---
+++ week_3/third_10/exercise 3_4_4_12.py
@@ -0,0 +1,5 @@
+numbers = [18, 310, 612, 193, 704]
+total = 0
+for number in numbers:
+    total += number**2
+print(total)
